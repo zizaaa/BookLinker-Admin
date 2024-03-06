2727ea9fde76fa42041fd6dfcc96e2cd938266ea
ziza <winfourteen@gmail.com>
2024-03-06 14:41:20 +0800

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import PieChart from '../components/charts/PieChart'
-import BorrowedBookGraph from '../components/charts/BorrowedBookGraph'
 import TransactionsHistory from '../components/tables/TransactionsHistory'
 
 function Dashboard() {
     
     return (
         <section className='drop-shadow-md'>
-            <PieChart/>
-            {/* <BorrowedBookGraph/> */}
             <TransactionsHistory/>
         </section>
     )
